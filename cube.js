f00aed45ee42b48859d3153b790bf80c64dd35b6
yeonkyujeon <ehb6915@naver.com>
2020-12-07 15:02:41 +0900

    Feat : 계산하는 함수 calculationOutput 구현하여  1단계 구현 완료

--- cube.js
+++ cube.js
@@ -1,4 +1,3 @@
-
 //Node.js 실행 모듈
 const useModule = () => {
   const readline = require('readline');
@@ -10,14 +9,15 @@ const useModule = () => {
 
   let input = [];
   rl.on("line", function (line) {
-    receiveInput(line);
+    input = receiveInput(line);
     rl.close();
   })
 
   rl.on("close", function () {
-    let A = input[0];
-    let B = input[1];
-    console.log(A + B);
+    const word = input[0];
+    const num = input[1];
+    const direction = input[2];
+    console.log(calculationOutput(word, num, direction));
   })
 }
 
@@ -26,7 +26,30 @@ const receiveInput = (line) => {
   return input;
 }
 
+const calculationOutput = (word, num, direction) => {
+  const wordArray = word.split('');
+  const number = Number(num);
+  let directionUppercase = direction.toUpperCase();
+  if (number < 0) {
+    if (directionUppercase === 'L') {
+      directionUppercase = 'R';
+    } else if (directionUppercase === 'R') {
+      directionUppercase = 'L';
+    }
+  }
+  for (let i = 0; i < Math.abs(number); i++) {
+    if (directionUppercase === 'L') {
+      const temp = wordArray.shift();
+      wordArray.push(temp);
+    } else if (directionUppercase === 'R') {
+      const temp = wordArray.pop();
+      wordArray.unshift(temp);
+    }
+  }
+  return wordArray.join('');
+}
 
 //Node.js 실행하기
 console.log('단어 하나, 정수 숫자하나, L또는 R을 공백으로 분리하여 순서대로 입력해주세요');
+console.log('ex) carrot -1 r');
 useModule();
